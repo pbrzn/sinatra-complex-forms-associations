5b1035d434fc3f5629c8e25cba5ea149f3a30bf0
Patrick Brennan <pbrennanmusic@gmail.com>
2021-07-04 20:52:42 -0400

    commit tree d2ed296ed82469ac6545bb9b23e0e84b016469cd

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 # Add seed data here. Seed your database with `rake db:seed`
 sophie = Owner.create(name: "Sophie")
+fran = Owner.create(name: "Francesca")
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
+Pet.create(name: "Cali", owner: fran)
